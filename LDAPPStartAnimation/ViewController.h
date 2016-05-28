4d42d9b0da0f4ddfd503b33c300500e1a098c43f
Daredos <asiosldh@163.com>
2016-05-28 10:23:51 +0800

    first commit

---
+++ LDAPPStartAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDAPPStartAnimation
+//
+//  Created by Daredos on 16/5/28.
+//  Copyright © 2016年 LiangDaHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LDAPPStartAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LDAPPStartAnimation
+//
+//  Created by Daredos on 16/5/28.
+//  Copyright © 2016年 LiangDaHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
